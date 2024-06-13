0ff3967480b3138db1f9cabe4b8173774b7cad98
sarkar9005 <90883518+sarkar9005@users.noreply.github.com>
2024-06-13 10:13:43 +0530

    Delete shop directory

--- shop/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Product
-
-# Register your models here.
-
-@admin.register(Product)
-class Admin(admin.ModelAdmin):
-    list_display = ['name', 'desc', 'price', 'img', 'que','subcategory',]
-    
