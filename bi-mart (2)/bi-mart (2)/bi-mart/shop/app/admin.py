5861cac1ab37bbcde0e64bbf4835613ab94829ed
sarkar9005 <90883518+sarkar9005@users.noreply.github.com>
2024-06-13 10:16:33 +0530

    Add files via upload

---
+++ bi-mart (2)/bi-mart (2)/bi-mart/shop/app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from . models import Product
+
+
+
+# Register your models here.
+
+@admin.register(Product)
+class Admin(admin.ModelAdmin):
+    list_display = ['name', 'desc', 'price', 'img', 'que','subcategory', 'category','id']
+
+    
+    
